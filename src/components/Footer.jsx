a0b83b0dea0431c39c1f5591d8a5bf8fb9bec3b5
olicauli <olicauli@protonmail.com>
2023-08-18 18:45:57 -0500

    create project files, fix footer on small screens

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="max-h-10 py-1 rounded flex justify-between border-red-400 border-t-4 my-auto">
       <p className="text-teal-100 pl-2 w-fit text-center text-xs  italic tracking-widest font-extralight">written by oliver davies, 2023-present.
-      background image by <a href="https://unsplash.com/photos/p0j-mE6mGo4">lorenza herrero from unsplash.</a></p>
+      <span className="text-teal-100 font-extralight text-xs italic hidden md:inline"> background image by <a href="https://unsplash.com/photos/p0j-mE6mGo4">lorenza herrero from unsplash.</a></span></p>
       <div className="text-teal-100 w-fit text-xs italic tracking-widest font-extralight">
         <a className="pr-2 w-fit text-xs" href="#">github</a>
         <a className="pr-2 w-fit text-xs" href="#">itch.io</a>
