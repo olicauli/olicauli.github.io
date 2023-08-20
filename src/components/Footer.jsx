8a381d81976ac96782d60fa87514eb4624673525
olicauli <olicauli@protonmail.com>
2023-08-20 05:40:11 -0500

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,12 @@
 import Socials from './Socials'
+import ExternalLink from './ExternalLink'
 
 const Footer = () => {
   return (
     <footer className="max-h-10 py-1 rounded flex justify-between border-red-400 border-t-4 my-auto">
       <p className="text-teal-100 pl-2 w-fit text-center text-xs  italic tracking-widest font-extralight">written by olicauli, 2023-present.
-      <span className="text-teal-100 font-extralight text-xs italic hidden md:inline"> all images from <a href="https://unsplash.com/">unsplash.</a></span></p>
+      <span className="text-teal-100 font-extralight text-xs italic hidden md:inline"> stock images from <ExternalLink linkUrl='https://www.unsplash.com' content='unsplash'/>.</span></p>
+
       <div className="text-teal-100 w-fit text-xs italic tracking-widest font-extralight footer-links">
         <Socials/>
       </div>
