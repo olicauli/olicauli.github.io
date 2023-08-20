101412c51978c2470e5fb73f79e12bfd238b7141
olicauli <olicauli@protonmail.com>
2023-08-20 05:47:45 -0500

    update project

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -9,7 +9,7 @@ const Project = ({title, desc, link, image}) => {
   }
 
   return (
-    <a href={link} className='project fade-in-3' style={backgroundImg}>
+    <a href={link} rel="noreferrer noopener" target="_blank" className='project fade-in-3' style={backgroundImg}>
       <div className='bg-black bg-gradient-to-b from-blue-950 to-transparent bg-opacity-30 h-full w-full' >
         <h2 className='text-center font-bold text-md mx-auto h-fit px-2 py-1 mb-1 bg-red-400 bg-opacity-100 rounded text-red-50 tracking-widest'>{ title }</h2>
         <p className='text-center px-1 text-red-50 font-normal italic text-sm tracking-widest text-ellipsis '> { desc }</p>
