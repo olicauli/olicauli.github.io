94d5a3f20425ebd4a0ad3e2e20faa1a3f9de7b2e
olicauli <olicauli@protonmail.com>
2023-08-20 02:06:12 -0500

    work on project list

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import PropTypes from 'prop-types';
 
-const Project = () => {
+const Project = ({title, desc, link, image}) => {
+
+  const backgroundImg = 
+  {
+    backgroundImage: 'url('+ image + ')',
+    backgroundSize: 'fit',
+  }
+
   return (
-    <div className='move-3'>
-        <p>placeholder</p>
-    </div>
+    <a href="#" className='fade-in-3 w-80 h-40  rounded-xl project text-ellipsis overflow-clip' style={backgroundImg}>
+      <div className='bg-red-950 bg-opacity-20 h-full w-full' >
+        <h2 className='text-center font-semibold mx-auto h-fit px-2 py-1 mb-1 bg-red-400 bg-opacity-100 rounded text-red-50 tracking-widest'>{ title }</h2>
+        <p className='text-center text-red-100 tracking-widest text-ellipsis '> { desc }</p>
+      </div>
+    </a>
   )
+} 
+
+Project.propTypes = 
+{
+  title: PropTypes.string,
+  desc: PropTypes.string,
+  link: PropTypes.string,
+  image: PropTypes.string,
 }
 
 export default Project
--- src/components/projects/ProjectDisplay.jsx
+++ src/components/projects/ProjectDisplay.jsx
@@ -1,9 +1,14 @@
 import Project from './Project'
 
+import projects from './project-list.json'
+
 const ProjectDisplay = () => {
   return (
-    <div>
-      <Project/> <Project/> <Project/> <Project/>
+    <div className='flex flex-wrap gap-5 pt-3 xs:justify-center sm:justify-start'>
+      {
+        projects.map((project) => (<Project key={project.title} title={project.title} desc={project.desc} image={project.image}/> ))
+      }
+      {/* <Project title={projects[0].title} desc={projects[0].desc} image={projects[0].image}/>  */}
     </div>
   )
 }
