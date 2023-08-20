ea718ecf0333897ab286e2acfac42357051dc6d1
olicauli <olicauli@protonmail.com>
2023-08-20 06:18:12 -0500

    make dist

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'docs'
+    outDir: 'dist'
   },
+  base: '/portfolio-site/',
   plugins: [
     react(),
   ],
