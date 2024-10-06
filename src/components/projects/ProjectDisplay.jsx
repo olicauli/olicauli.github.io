46d0ed7028813d8a419d4f596103668c85aa7874
oli <olivercauliflower@gmail.com>
2024-10-05 23:17:05 -0500

    update styling for project display

--- src/components/projects/ProjectDisplay.jsx
+++ src/components/projects/ProjectDisplay.jsx
@@ -3,7 +3,7 @@ import projects from './project-list.json'
 
 const ProjectDisplay = () => {
   return (
-    <div className='flex flex-wrap gap-5 lg:gap-10 py-3 lg:py-5 lg:justify-around '>
+    <div className='flex flex-wrap gap-5 lg:gap-7 py-3 lg:py-5 lg:justify-items-center justify-between '>
       {
         projects.map((project) => (<Project key={project.title} title={project.title} link={project.link} desc={project.desc} image={project.image}/> ))
       }
